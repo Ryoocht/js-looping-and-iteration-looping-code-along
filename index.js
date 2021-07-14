e5954adc190e64bbdd5942a5468dc334ca9d3e41
Ryochi <ryo1.t.zubu@gmail.com>
2021-07-14 17:10:18 +1000

    Done with assignemnt

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(array, event){
+    const message = [];
+    for (let i = 0; i < array.length; i++) {
+        message.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+    }
+    return message;
+}
+
+function countDown(num){
+    let count = num;
+    while(count >= 0){
+        console.log(count);
+        count--;
+    }
+}
